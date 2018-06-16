9b78bb5663069a2eaff405694d1b1c2571ff33f8
Sérgio Ribeiro <sergioribeiro@id.uff.br>
2018-06-16 20:53:42 -0300

    controle estrategias correto

--- TesteEstrategia/TesteEstrategia.ino
+++ TesteEstrategia/TesteEstrategia.ino
@@ -4,7 +4,11 @@ int ledSign = 13;
 int counter = 0;
 int selectState = 0;
 int startState = 0;
+int currentState = 0;
+int previousState = 0;
 double teste;
+int lastButtonState = 0;  
+bool bPress = false;
 
 void setup() {
   Serial.begin(9600);
@@ -18,16 +22,31 @@ void setup() {
   
 void loop() {
   selectState = digitalRead(botaoSelect);
+  currentState = selectState;
   startState = digitalRead(botaoStart);
-  if(selectState == LOW){
-    counter = counter + 1;
-    Serial.println(counter);
-  }
-  if(startState == LOW){
-    Serial.print("Contagem: ");
-    Serial.println(counter/50);
- //   while(counter != 0){
-      
-  //  }
-  }
+  countButton();
+  if( bPress){
+    bPress = false;
+   }  
+   if(startState == LOW){
+    
+   }
+   if(startState == LOW){ //AQUI O ROBÔ DÁ O PLAY
+      Serial.print("Contagem: ");
+      Serial.println(counter);  
+   }
 }
+
+void countButton(){
+  selectState = digitalRead(botaoSelect);
+  if (selectState != lastButtonState) {
+    
+    if (selectState == LOW) {
+      bPress = true;
+      counter++;   
+    }
+    delay(50);
+  }
+  lastButtonState = selectState;
+}
+
